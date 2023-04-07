bcf54bc12c0a4a0f44443f3c1de7efc02eed6044
Khanh  Nam <125676795+nemmm207@users.noreply.github.com>
2023-04-07 11:28:56 +0700

    Update CTDL&GT_Binary_Recursion_1.cpp

--- CTDL&GT_Binary_Recursion_1.cpp
+++ CTDL&GT_Binary_Recursion_1.cpp
@@ -5,11 +5,3 @@ int fib(int n){
   if(n <= 2) return 1;
   return fib(n - 1) + fib(n - 2);
 }
-int main() {
-  int kq,n;
-  cout<<"Nhập vị trí n muốn tìm trong dãy fibonacci: ";
-  cin>>n;
-  kq = fib(n);
-  cout<<"\nSố fibonacci ở vị trí "<<n<<" là: "<<kq;
-}
-
